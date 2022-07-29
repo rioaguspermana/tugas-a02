f9a5c7b44de479cd63f37554e68195f2ecbc5a00
Rio Agus Permana <rap.sherlock@gmail.com>
2022-07-29 13:12:57 +0700

    modify form contact

--- js/script.js
+++ js/script.js
@@ -39,15 +39,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
         updateTextTerminal()
     }, 5500);
 
-    // === START HERO ANIMATION ON SCROLL
+    // === START ANIMATION ON SCROLL
+    // hero el
     const heroElement = document.getElementById("hero");
     const heroLeftElement = document.getElementsByClassName("left-hero")?.[0];
     const heroRightH1 = document.querySelector("#hero section.right-hero h1");
     const heroRightP = document.querySelector("#hero section.right-hero p");
     const heroRightA = document.querySelector("#hero section.right-hero a");
+    // contact el
+    const contactElement = document.getElementById("form_contact");
+    const contactElementInfo = document.querySelector("#form_contact .content-contact .contact-info");
+    const contactElementForm = document.querySelector("#form_contact .content-contact .contact-form");
     window.addEventListener("scroll", (event) => {
         const scrollPosition = window.scrollY;
-        console.log(scrollPosition);
+        // console.log(scrollPosition);
         // animated terminal (left hero section)
         if (scrollPosition <= heroElement.offsetHeight) {
             if (scrollPosition <= 180) {
@@ -55,9 +60,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
         }
         // animated right hero section H1 / P / A
-        scrollPosition > (heroElement.offsetHeight / 6) ? heroRightH1.classList.add("fade-out-right") : heroRightH1.classList.remove("fade-out-right")
-        scrollPosition > ((heroElement.offsetHeight / 6) + 100) ? heroRightP.classList.add("fade-out-right") : heroRightP.classList.remove("fade-out-right")
-        scrollPosition > ((heroElement.offsetHeight / 6) + 150) ? heroRightA.classList.add("fade-out-right") : heroRightA.classList.remove("fade-out-right")
+        scrollPosition > (heroElement.offsetHeight / 6) ? heroRightH1.classList.add("fade-out-right") : heroRightH1.classList.remove("fade-out-right");
+        scrollPosition > ((heroElement.offsetHeight / 6) + 100) ? heroRightP.classList.add("fade-out-right") : heroRightP.classList.remove("fade-out-right");
+        scrollPosition > ((heroElement.offsetHeight / 6) + 150) ? heroRightA.classList.add("fade-out-right") : heroRightA.classList.remove("fade-out-right");
+
+        // start element animation on scroll for contact
+        if ((window.scrollY - contactElement.offsetTop) > -500) {
+            (window.scrollY - contactElement.offsetTop) > (contactElement.offsetHeight / 2) || (window.scrollY - contactElement.offsetTop) < -280 ? contactElementInfo.classList.add("fade-out-right") : contactElementInfo.classList.remove("fade-out-right");
+            (window.scrollY - contactElement.offsetTop) > ((contactElement.offsetHeight / 2) + 100) || (window.scrollY - contactElement.offsetTop) < -380 ? contactElementForm.classList.add("fade-out-right") : contactElementForm.classList.remove("fade-out-right");
+        }
     });
+    // === END ANIMATION ON SCROLL
 
 });
